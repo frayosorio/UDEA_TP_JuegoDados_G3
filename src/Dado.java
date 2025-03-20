a8afd4ff54f9bbb8ece82ecd51b9191493e6eaee
fray osorio <frayosorio@gmail.com>
2025-03-20 13:47:23 -0500

    Se crea la interfaz grafica y se agrega la clase DADO

---
+++ src/Dado.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Dado {
+
+    private int numero;
+
+    public void lanzar(Random r) {
+        // el numero de ser un valor al azar entre 1 y 6
+        numero = r.nextInt(6) + 1;
+    }
+
+    public void mostrar(JLabel lbl) {
+        String nombreArchivo = "/imagenes/" + numero + ".jpg";
+        ImageIcon imgDado = new ImageIcon(getClass().getResource(nombreArchivo));
+
+        lbl.setIcon(imgDado);
+    }
+
+}
